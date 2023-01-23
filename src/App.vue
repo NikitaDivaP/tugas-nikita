0b79be52a8ebf2fdddebc9a02cd5ef3e60e12b26
Gempar13 <bustangempar@gmail.com>
2023-01-23 19:38:21 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default{
+
+  data(){
+    return{
+      datasensor: "60",
+      datasensor2: "80"
+    }
+  },
+  created(){
+    this.getData2()
+  },
+  methods:{
+    getData(){
+      this.datasensor = 100
+    },
+    getData2(){
+      //alert('hello 2');
+    },
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    Hello {{ datasensor }} {{ datasensor2 }}
+    <button @click="getData" class="bg-orange-500 p-2 rounded-sm">Ambil Data</button>&nbsp;
+    <button @click="getData" class="bg-orange-500 p-2 rounded-sm">Ambil Data 2</button>
+  </div>
+</template>
+
